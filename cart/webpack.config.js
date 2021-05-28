ad546f9660b56561f0a731121195fdabfcd8f1d0
Shreeansh <sarojvarshney20@gmail.com>
2021-05-28 11:48:12 +0530

    added cart service

---
+++ cart/webpack.config.js
@@ -0,0 +1,21 @@
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+
+module.exports = {
+  mode: 'development',
+  devServer: {
+    port: 4001,
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: './public/index.html',
+    }),
+    new ModuleFederationPlugin({
+      name: 'cart',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './CartIndex': './src/index.js',
+      },
+    }),
+  ],
+};
